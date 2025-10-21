c619fb41ea08dcba35aa242aeb7f652291f40935
Dimen2 <dima09445@gmail.com>
2025-10-21 12:14:10 +0300

    Updates

--- src/Contacts.js
+++
@@ -1,9 +0,0 @@
-export default function Contacts() {
-  return (
-    <div className="profile-page">
-      <h2>📞 Contacts</h2>
-      <p>Мой номер: +380 ХХХ ХХХ ХХХХ</p>
-      <p>Telegram: @твой_канал</p>
-    </div>
-  );
-}
